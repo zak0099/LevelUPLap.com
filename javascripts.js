21d367576d2326b39c5526d2ae273075e3176a63
zak0099 <khnzariab143@gmail.com>
2024-08-08 18:46:55 +0500

    Add files via upload

---
+++ javascripts.js
@@ -0,0 +1,17 @@
+// Add event listener to logo image
+document.getElementById('logo').addEventListener('click', function() {
+    console.log('Logo clicked!');
+});
+
+// Add event listener to game images
+document.getElementById('game1').addEventListener('click', function() {
+    console.log('Game 1 clicked!');
+});
+
+document.getElementById('game2').addEventListener('click', function() {
+    console.log('Game 2 clicked!');
+});
+
+document.getElementById('game3').addEventListener('click', function() {
+    console.log('Game 3 clicked!');
+});
